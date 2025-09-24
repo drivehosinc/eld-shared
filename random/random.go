325283f0e5d37179215c0c6a8d3816e0a5435a1e
zfirdavs <firdavs792@gmail.com>
2025-09-24 17:54:15 +0500

    feat: add the argon2 and random packages

---
+++ random/random.go
@@ -0,0 +1,12 @@
+package random
+
+import "crypto/rand"
+
+func GenerateRandomBytes(n uint32) ([]byte, error) {
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
