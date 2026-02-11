bc39e66095694640bafa0829e2f85753b9effe60
Jakhongir <anorboev.jahongir8007@gmail.com>
2026-02-11 17:47:04 +0500

    fix

---
+++ postgresql/interface.go
@@ -0,0 +1,29 @@
+package postgresql
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// ClientI is the full Client interface excluding lifecycle methods (Ping, Close).
+type ClientI interface {
+	Querier
+	Pool(ctx context.Context) *pgxpool.Pool
+	Begin(ctx context.Context) (pgx.Tx, error)
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+	WithTransaction(ctx context.Context, fn func(ctx context.Context, tx pgx.Tx) error) error
+	CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error)
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+}
+
+type Querier interface {
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+}
+
+// compile-time check
+var _ ClientI = (*Client)(nil)
